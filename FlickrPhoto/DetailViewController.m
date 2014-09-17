9554b547659fb8b10de196d5f7bb3f78c7668834
Frederick C. Lee <frederick.c.lee@gmail.com>
2014-09-16 19:02:35 -0700

    Added DetailViewController.

---
+++ FlickrPhoto/DetailViewController.m
@@ -0,0 +1,61 @@
+//
+//  DetailViewController.m
+//  FlickrPhoto
+//
+//  Created by Frederick C. Lee on 9/16/14.
+//  Copyright (c) 2014 Frederick C. Lee. All rights reserved.
+//
+
+#import "DetailViewController.h"
+#import "ImageDownloader.h"
+#import "MainViewController.h"
+
+
+@interface DetailViewController ()
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+@property (weak, nonatomic) MainViewController *mainViewController;
+
+@end
+
+@implementation DetailViewController
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    [self displayBigImage];
+}
+
+// -----------------------------------------------------------------------------------------------------------------------
+
+- (void)willMoveToParentViewController:(UINavigationController *)navController {
+    if (navController) {
+        self.mainViewController = [navController viewControllers][0];
+    } else {
+        self.currentImageDownloader.bigImage = self.imageView.image;
+        _mainViewController.currentImageDownloader = _currentImageDownloader;
+    }
+}
+
+// -----------------------------------------------------------------------------------------------------------------------
+
+- (void)displayBigImage {
+    
+    ImageDownloaderCompletionBlock completion = ^(UIImage *image, NSError *error) {
+        if (image) {
+            self.imageView.image = image;
+        } else {
+            NSLog(@"%s: Error: %@", __PRETTY_FUNCTION__,
+                  [error localizedDescription]);
+        }
+    };
+    
+    UIImage *image = [_currentImageDownloader bigImage];
+    
+    if (image) {
+        self.imageView.image = image;
+    } else {
+        NSURL *URL = [NSURL URLWithString:_currentImageDownloader.dict[@"url_m"]];
+        [_currentImageDownloader downloadImageAtURL:URL completion:completion];
+    }
+}
+
+@end
---
+++ FlickrPhoto/MainViewController.m
@@ -0,0 +1,149 @@
+//  MainViewController.m
+//  FlickrPhoto
+//
+//  Created by Frederick C. Lee on 9/14/14.
+//  Copyright (c) 2014 Frederick C. Lee. All rights reserved.
+// -----------------------------------------------------------------------------------------------------------------------
+
+#import "MainViewController.h"
+#import "SimpleFlickrAPI.h"
+#import "ImageDownloader.h"
+#import "DetailViewController.h"
+
+@interface MainViewController () <UICollectionViewDataSource, UICollectionViewDelegate>
+@property (weak, nonatomic) IBOutlet UICollectionView *collectionView;
+@property (nonatomic, strong) NSMutableArray *downloaders;
+@property (nonatomic, strong) NSArray *photoInfos;
+@end
+
+@implementation MainViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self fetchFlickrPhotoWithSearchString:@"Ric"];
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    if (_currentImageDownloader) {
+        [_downloaders replaceObjectAtIndex:_selectedItemIndex withObject:_currentImageDownloader];
+    }
+}
+
+// -----------------------------------------------------------------------------------------------------------------------
+
+- (void)fetchFlickrPhotoWithSearchString:(NSString *)searchString {
+    
+    SimpleFlickrAPI *flickr = [SimpleFlickrAPI new];
+    
+    
+    [[[NSURLSession sharedSession] dataTaskWithURL:[flickr getURLForString:@"Ric"]
+                                 completionHandler:^(NSData *data,
+                                                     NSURLResponse *response,
+                                                     NSError *error) {
+                                     if (!error) {
+                                         NSString *string = [flickr stringByRemovingFlickrJavaScript:data];
+                                         NSData *jsonData = [string dataUsingEncoding:NSUTF8StringEncoding];
+                                         NSDictionary *jsonDict = [NSJSONSerialization JSONObjectWithData:jsonData
+                                                                                                  options:NSJSONReadingAllowFragments
+                                                                                                    error:&error];
+                                         
+                                         self.photoInfos = [[jsonDict objectForKey:@"photos"] objectForKey:@"photo"];
+                                         
+                                         NSMutableArray *downloaders = [[NSMutableArray alloc] initWithCapacity:[_photoInfos count]];
+                                         for (NSInteger index = 0; index < [_photoInfos count]; index++) {
+                                             ImageDownloader *downloader = [[ImageDownloader alloc] initWithDict:_photoInfos[index]];
+                                             [downloaders addObject:downloader];
+                                         }
+                                         self.downloaders = downloaders; // ...link local array with instance array 'downloaders' (Note: same object!).
+                                         
+                                         dispatch_async(dispatch_get_main_queue(), ^{
+                                             [self.collectionView reloadData];
+                                         });
+                                     } else {
+                                         NSLog(@"*** {SessionDataError}: %@",error);
+                                         // ...Handle Error.
+                                     }
+                                 }] resume];
+    
+}
+
+// -----------------------------------------------------------------------------------------------------------------------
+#pragma mark - UICollectionViewDelegate methods
+// ...to be implemented later.
+
+// -----------------------------------------------------------------------------------------------------------------------
+#pragma mark - UICollectionViewDataSource methods
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
+    return [self.photoInfos count];
+}
+
+// -----------------------------------------------------------------------------------------------------------------------
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
+    
+    UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"PhotoCell" forIndexPath:indexPath];
+    
+    UIImageView *photoImageView = (UIImageView *)[cell viewWithTag:1];
+    
+    // •(1) Each cell displays an image of the photo from Flickr.
+    //    However, the image must be downloaded first.
+    //    The completion block for the ImageDownloader object is defined.
+    //    The completion block sets the image property for cell’s photoImageView to the image passed in to the block.•
+    //    ImageDownloader(completion) --> ViewController(completion)
+    
+    
+    ImageDownloaderCompletionBlock completion = ^(UIImage *image, NSError *error) {                                  // 1
+        if (image) {
+            photoImageView.image = image;
+        } else {
+            NSLog(@"%s: Error: %@", __PRETTY_FUNCTION__,
+                  [error localizedDescription]);
+        }
+    };
+    
+    _selectedItemIndex = [indexPath item];   // ...NSIndexPath (UICollectionViewAdditions)
+    
+    self.currentImageDownloader = [_downloaders objectAtIndex:_selectedItemIndex];
+    
+    // (2) If the image was previously downloaded, the cell’s image is set right away.
+    // There is no reason to download it again.
+    
+    UIImage *image = [_currentImageDownloader image];                                 // 2
+    
+    if (image) {
+        photoImageView.image = image;
+    } else {
+        NSURL *URL = [NSURL URLWithString: _photoInfos[_selectedItemIndex][@"url_sq"]];
+        [_currentImageDownloader downloadImageAtURL:URL completion:completion];
+    }
+    
+    return cell;
+}
+
+
+// -----------------------------------------------------------------------------------------------------------------------
+#pragma mark - Segue Handler
+
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(UICollectionViewCell *)sender {
+    if ([[segue identifier] isEqualToString:@"showDetail"]) {
+        NSIndexPath *selectedIndexPath = [self.collectionView indexPathsForSelectedItems][0];
+        DetailViewController *detailViewController = [segue destinationViewController];
+        _selectedItemIndex = [selectedIndexPath item];
+        detailViewController.currentImageDownloader = [_downloaders objectAtIndex:_selectedItemIndex];
+    }
+}
+
+// -----------------------------------------------------------------------------------------------------------------------
+#pragma mark - Action methods
+
+- (IBAction)doSomethingAction:(id)sender {
+    return;
+}
+
+- (IBAction)exitAction:(id)sender {
+    exit(0);
+}
+
+@end
