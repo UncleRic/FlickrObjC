5e5dec3f988ef34f8edf43658ec76a38d44256d2
Frederick C. Lee <amourinetech@gmail.com>
2018-02-11 13:02:13 -0800

    Removed Vestigial Methods.

--- FlickrPhoto/SimpleFlickrAPI.h
+++ FlickrPhoto/SimpleFlickrAPI.h
@@ -13,17 +13,4 @@
 
 - (NSURL *)getURLForString:(NSString *)str;
 
-// Returns a set of photos matching the search string.
-- (NSArray *)photosWithSearchString:(NSString *)string;
-
-// Returns the Flickr NSID for the given user name.
-- (NSString *)userIdForUsername:(NSString *)username;
-
-// Returns a Flickr photo set for the user. userId is the Flickr NSID
-// of the user.
-- (NSArray *)photoSetListWithUserId:(NSString *)userId;
-
-// Returns the photos for a Flickr photo set.
-- (NSArray *)photosWithPhotoSetId:(NSString *)photoSetId;
-
 @end
